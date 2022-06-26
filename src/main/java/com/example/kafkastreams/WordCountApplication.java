4652c49f1ac7c8bd531998ece128c14f856e2ec8
Park <qkrtjddlf11@naver.com>
2022-06-26 16:07:08 +0900

    Kafka-Streams Example with Gradle

---
+++ src/main/java/com/example/kafkastreams/WordCountApplication.java
@@ -0,0 +1,52 @@
+package com.example.kafkastreams;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class WordCountApplication {
+
+    public static void main(final String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "172.30.1.210:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        // Serializers/deserializers (serde) for String and Long types
+        final Serde<String> stringSerde = Serdes.String();
+        final Serde<Long> longSerde = Serdes.Long();
+
+        // Construct a `KStream` from the input topic "streams-plaintext-input", where message values
+        // represent lines of text (for the sake of this example, we ignore whatever may be stored
+        // in the message keys).
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> textLines = builder.stream(
+                "streams-plaintext-input",
+                Consumed.with(stringSerde, stringSerde)
+        );
+
+        KTable<String, Long> wordCounts = textLines
+                // Split each text line, by whitespace, into words.
+                .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+
+                // Group the text words as message keys
+                .groupBy((key, value) -> value)
+
+                // Count the occurrences of each word (message key).
+                .count();
+
+        // Store the running counts as a changelog stream to the output topic.
+        wordCounts.toStream().to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+    }
+
+}
